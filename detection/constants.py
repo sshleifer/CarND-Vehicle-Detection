e91884a6e84dbb48b373c1e24a3fa862966fefad
sshleifer <sam@kensho.com>
2017-10-07 15:05:57 -0400

    Classifier gets high acc, but sucks on test_images

---
+++ detection/constants.py
@@ -0,0 +1,12 @@
+PARAMS = {
+  'cell_per_block': 2,
+  'color_space': 'RGB',
+  'hist_bins': 16,
+  # 'hist_feat': True,
+  'hog_channel': 0,
+  # 'hog_feat': True,
+  'orient': 9,
+  'pix_per_cell': 8,
+  # 'spatial_feat': True,
+  'spatial_size': (16, 16)
+ }
---
+++ detection/search_classify.py
@@ -0,0 +1,61 @@
+from detection.lesson_functions import *
+from lane_lines.nb_start import *
+
+import cv2
+# Define a function to extract features from a single image window
+# This function is very similar to extract_features()
+# just for a single image rather than list of images
+def single_img_features(img, color_space='RGB', spatial_size=(16, 16),
+                        hist_bins=32, orient=9,
+                        pix_per_cell=8, cell_per_block=2, hog_channel=0):
+
+    feature_image = convert_color(img, color_space)
+    feature_image = cv2.resize(feature_image, (64, 64))
+    # 3) Compute spatial features if flag is set
+
+    spatial_features = bin_spatial(feature_image, size=spatial_size)
+    hist_features = color_hist(feature_image, nbins=hist_bins)
+
+    if hog_channel == 'ALL':
+        hog_features = []
+        for channel in range(feature_image.shape[2]):
+            hog_features.extend(get_hog_features(feature_image[:, :, channel],
+                                                 orient, pix_per_cell, cell_per_block,
+                                                 vis=False, feature_vec=True))
+    else:
+        hog_features = get_hog_features(feature_image[:, :, hog_channel], orient,
+                                        pix_per_cell, cell_per_block, vis=False,
+                                        feature_vec=True)
+
+    # 9) Return concatenated array of features
+    return np.concatenate([spatial_features, hist_features, hog_features])
+
+
+
+def search_windows(img, windows, clf, color_space='RGB',
+                   spatial_size=(32, 32), hist_bins=32, orient=9,
+                   pix_per_cell=8, cell_per_block=2,
+                   hog_channel=0, spatial_feat=True,
+                   hist_feat=True, hog_feat=True):
+    '''make features and predict on each window'''
+    # 1) Create an empty list to receive positive detection windows
+    on_windows = []
+    # 2) Iterate over all windows in the list
+    for window in windows:
+        # 3) Extract the test window from original image
+        test_img = cv2.resize(img[window[0][1]:window[1][1], window[0][0]:window[1][0]],
+                              (64, 64))
+        # 4) Extract features for that window using single_img_features()
+        features = single_img_features(test_img, color_space=color_space,
+                                       spatial_size=spatial_size, hist_bins=hist_bins,
+                                       orient=orient, pix_per_cell=pix_per_cell,
+                                       cell_per_block=cell_per_block,
+                                       hog_channel=hog_channel)
+        # 6) Predict using your classifier
+
+        prediction = clf.predict(features.reshape(1,-1))
+        # 7) If positive (prediction == 1) then save the window
+        if prediction == 1:
+            on_windows.append(window)
+    # 8) Return windows for positive detections
+    return on_windows
