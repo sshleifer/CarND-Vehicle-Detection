87e75e7e17853042dc8300cc388cde32e752c828
sshleifer <sam@kensho.com>
2017-10-08 18:09:36 -0400

    boom boom

--- detection/video.py
+++ detection/video.py
@@ -26,8 +26,20 @@ def video_pipeline(clip, clf, threshold=1, search_params=PARAMS):
         windows = make_many_windows(image)
         hot_windows = search_windows(image, windows, clf, **search_params)
         heatmap = get_heatmap(hot_windows, image, threshold=threshold)
+        keep = []
+        if len(last_hot_windows) < 5:
+            keep = heatmap
+        else:
+            for w in last_hot_windows[-5:]:
+                for cand in heatmap:
+                    print(cand, w)
+                    if cand in w:
+                        keep.append(cand)
+
+        #last_hot_windows.
         # window_img = draw_boxes(image, hot_windows, color=(0, 0, 255), thick=6)
-        output = draw_heat(image, heatmap)
+        output = draw_heat(image, keep)
+        last_hot_windows.append(keep)
         imgs.append(output)
 
     return imgs#, diag
